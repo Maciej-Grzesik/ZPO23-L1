ebc49b341c8db9b02f2026045ef7c596a72066f2
Maciej Grzesik <124913584+Maciejkrul@users.noreply.github.com>
2023-10-16 23:48:57 +0200

    Update Ex6.java
    zamiana kolejnosci statementów if w celu wyeliminowania bledu gdzie dopuszczalnyDzien = 30 nie ma szans zajcia

--- Błędne/Ex6.java
+++ Błędne/Ex6.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+/**
+* Klasa do formatowania daty DD-MM-RRRR
+*/ 
 public class Ex6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,23 +19,25 @@ public class Ex6 {
         }
     }
 
+    /**
+    * Metoda sprawdzajaca poprawnosci wpisanych danych i zwracajaca date w formacie DD-MM-RRRR
+    */
     public static String data(int dzien, int miesiac, int rok) {
-        int dopuszczalnyDzien = 31;
+        int dopuszczalnyDzien;
 
-        if (miesiac < 1 || miesiac > 12) {
+        if (miesiac >= 1 && miesiac <= 12) {
             throw new IllegalArgumentException("Podano nieprawidlowy miesiac");
         }
-
-        if (miesiac == 4 || miesiac == 6 || miesiac == 9 || miesiac == 11) {
-            dopuszczalnyDzien = 30;
-        }
-
+        
         if (miesiac == 2) {
             dopuszczalnyDzien = 29;
         } else {
             dopuszczalnyDzien = 31;
         }
-
+        if (miesiac == 4 || miesiac == 6 || miesiac == 9 || miesiac == 11) {
+            dopuszczalnyDzien = 30;
+        }
+        
         if ((dzien < 1) || (dzien >= dopuszczalnyDzien)) {
             throw new IllegalArgumentException("Podano nieprawidłowy dzień.");
         }
