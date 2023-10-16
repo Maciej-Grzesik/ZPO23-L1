fbcfc45a7986065aacc5a9f7ad7aa349592fa931
Maciej Grzesik <124913584+Maciejkrul@users.noreply.github.com>
2023-10-16 23:25:40 +0200

    Update Ex3.java
    poprawienie kolejnosci rbg na rgb, zamiana powtorzonego s2 na s3

--- Błędne/Ex3.java
+++ Błędne/Ex3.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+/**
+* Klasa konwertujaca kolor w formacie HEX do RGB
+*/
 public class Ex3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,8 +17,8 @@ public class Ex3 {
 
         int r = Integer.parseInt(s1, 16);
         int g = Integer.parseInt(s2, 16);
-        int b = Integer.parseInt(s2, 16);
-        System.out.printf("(" + r + "," + b + "," + g + ")");
+        int b = Integer.parseInt(s3, 16);
+        System.out.printf("(" + r + "," + g + "," + b + ")");
 
     }
 }
