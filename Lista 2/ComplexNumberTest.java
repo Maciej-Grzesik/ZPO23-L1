916226e85742fc4abb2ef810a2e2efa2f390dded
Maciej Grzesik <124913584+Maciejkrul@users.noreply.github.com>
2023-11-17 22:17:22 +0100

    Update ComplexNumberTest.java
    Zamienienie Klasy testującej na surowych wartościach tak aby obsługiwała testy jednostkowe przy pomocy jupiter

--- Lista 2/ComplexNumberTest.java
+++ Lista 2/ComplexNumberTest.java
@@ -1,35 +1,127 @@
-/**
- * Klasa testujaca
- */
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import static org.junit.jupiter.api.Assertions.*;
+
 class ComplexNumberTest {
-    public static void main(String[] args) {
-        ComplexNumber z = new ComplexNumber(1, 2);
-        ComplexNumber z1 = new ComplexNumber(3, 4);
-        Vector2D v = new Vector2D(1, 1);
-        System.out.println("Wektor: " + v);
-        System.out.format("Modul: %.5f", v.getModule());
-        System.out.println();
-        System.out.format("Kat: %.5f%n", v.getArgument());
-        System.out.println("Liczba zespolona z = " + z);
-        System.out.format("Modul liczby zesplonej |z| = %.5f%n", z.getModule());
-        System.out.format("Kat liczby zespolonej z: %.5f[rad] %n", z.getArgument());
-        System.out.println("Re(z) + Im(z)i: " + z.exp());
-        System.out.println("Liczba z w postaci biegunowej: " + z.polar());
-        System.out.println("z + z1 = " + ComplexNumber.add(z, z1));
-        System.out.println("z - z1 = " + ComplexNumber.subtract(z, z1));
-        System.out.println("z * z1 = " + ComplexNumber.multiply(z, z1));
-        try {
-            System.out.println("z : z1 = " + ComplexNumber.divide(z, z1));
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
-        ComplexNumber z2 = new ComplexNumber(0, 0);
-        try {
-            System.out.println("z : z1 = " + ComplexNumber.divide(z, z2));
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println("z^4 = " + z.power(4));
-        System.out.println(z.equals(z1));
+
+    @Test
+    void testAdd() {
+        ComplexNumber cn1 = new ComplexNumber(3, 4);
+        ComplexNumber cn2 = new ComplexNumber(1, 2);
+        ComplexNumber result = ComplexNumber.add(cn1, cn2);
+        assertEquals(4, result.Re());
+        assertEquals(6, result.Im());
     }
+
+    @Test
+    void testSubtract() {
+        ComplexNumber cn1 = new ComplexNumber(3, 4);
+        ComplexNumber cn2 = new ComplexNumber(1, 2);
+        ComplexNumber result = ComplexNumber.subtract(cn1, cn2);
+        assertEquals(2, result.Re());
+        assertEquals(2, result.Im());
+    }
+
+    @Test
+    void testMultiply() {
+        ComplexNumber cn1 = new ComplexNumber(3, 2);
+        ComplexNumber cn2 = new ComplexNumber(1, 4);
+        ComplexNumber result = ComplexNumber.multiply(cn1, cn2);
+        assertEquals(-5, result.Re());
+        assertEquals(14, result.Im());
+    }
+
+    @Test
+    void testDivide() {
+        ComplexNumber cn1 = new ComplexNumber(3, 2);
+        ComplexNumber cn2 = new ComplexNumber(1, 4);
+        ComplexNumber result = ComplexNumber.divide(cn1, cn2);
+        assertEquals(0.647, result.Re(), 0.01);
+        assertEquals(-0.588, result.Im(), 0.01);
+    }
+
+    @Test
+    void testPower() {
+        ComplexNumber cn = new ComplexNumber(3, 2);
+        ComplexNumber result = cn.power(3);
+        assertEquals(-9, result.Re(), 0.01);
+        assertEquals(46, result.Im(), 0.01);
+    }
+
+    @Test
+    void testEquals() {
+        ComplexNumber cn1 = new ComplexNumber(3, 4);
+        ComplexNumber cn2 = new ComplexNumber(3, 4);
+        assertTrue(cn1.equals(cn2));
+    }
+
+    @Test
+    void testToString() {
+        ComplexNumber cn = new ComplexNumber(3, 4);
+        assertEquals("3.0 + i4.0", cn.toString());
+    }
+
+    @Test
+    void testDivideByZero() {
+        ComplexNumber cn1 = new ComplexNumber(3, 4);
+        ComplexNumber cn2 = new ComplexNumber(0, 0);
+        assertThrows(ArithmeticException.class, () -> ComplexNumber.divide(cn1, cn2));
+    }
+
+    @Test
+    void testInput() throws InvalidComplexNumberFormat {
+        ByteArrayInputStream in = new ByteArrayInputStream("3-i5".getBytes());
+        System.setIn(in);
+        ComplexNumber result = ComplexNumber.Input();
+        assertEquals(3, result.Re());
+        assertEquals(-5, result.Im());
+    }
+
+    @Test
+    void testInvalidComplexNumberFormat() {
+        // Przygotowanie danych do zasymulowania nieprawidłowego formatu liczby zespolonej
+        ByteArrayInputStream in = new ByteArrayInputStream("1++i1".getBytes());
+        System.setIn(in);
+
+        // Sprawdzenie, czy metoda rzuci oczekiwany wyjątek dla nieprawidłowego formatu
+        assertThrows(InvalidComplexNumberFormat.class, ComplexNumber::Input);
+    }
+    // Dodaj testy dla metody Input() w przypadku poprawnego i niepoprawnego formatu liczby zespolonej
+    @Test
+    void testExp() {
+        ComplexNumber cn = new ComplexNumber(3, 4);
+        assertEquals(cn.exp(), "5.0e(0.9272952180016122i)");
+    }
+
+    @Test
+    void testPolar() {
+        ComplexNumber cn = new ComplexNumber(3, 4);
+        assertEquals("z = (5.0,0.9272952180016122)", cn.polar());
+    }
+
+    @Test
+    void testPowerZero() {
+        ComplexNumber cn = new ComplexNumber(3, 4);
+        ComplexNumber result = cn.power(0);
+        assertEquals(1, result.Re());
+        assertEquals(0, result.Im());
+    }
+
+    @Test
+    void testPowerNegative() {
+        ComplexNumber cn = new ComplexNumber(3, 2);
+        ComplexNumber result = cn.power(-2);
+        assertEquals(0.0295, result.Re(), 0.001);
+        assertEquals(-0.0710, result.Im(), 0.001);
+    }
+
+    @Test
+    void testInputWithZeroImaginaryPart() throws InvalidComplexNumberFormat {
+        ByteArrayInputStream in = new ByteArrayInputStream("3".getBytes());
+        System.setIn(in);
+        ComplexNumber result = ComplexNumber.Input();
+        assertEquals(3, result.Re());
+        assertEquals(0, result.Im());
+    }
+
 }
