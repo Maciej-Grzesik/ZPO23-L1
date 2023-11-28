2e2ddeec104f701ddbe73beffcc573011383811f
Maciej Grzesik <124913584+Maciejkrul@users.noreply.github.com>
2023-11-28 22:00:07 +0100

    dodanie liczenia wyrazow klasy optional i i funkcje lambda

--- Lista 5/Statistic.java
+++ Lista 5/Statistic.java
@@ -1,32 +1,42 @@
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Statistic {
-    private final Map<String, Integer> characters_unique;
-    private final Map<String, Integer> words_unique;
-    private final List<String> characters_count;
-    private final List<String> words_count;
+    private final Map<String, Long> charactersUnique;
+    private final Map<String, Long> wordsUnique;
+    private final List<String> charactersCount;
+    private final List<String> wordsCount;
 
-    public Statistic(String sentence){
-        this.characters_unique = new HashMap<>();
-        this.words_unique = new HashMap<>();
-        this.characters_count = new ArrayList<>(Arrays.asList(sentence.trim().split("(?!^)")));
-        this.words_count = new ArrayList<>(Arrays.asList(sentence.trim().split("\\s+")));
-    }
-    public Integer countTotalWords(){
-        return words_count.size();
-    }
-    public Integer countUniqueWords(){
-        for (String word : words_count){
-            words_unique.put(word.toLowerCase(), words_unique.getOrDefault(word, 0 ) + 1);
-        } return words_unique.size();
-    }
-    public Integer countTotalCharacters(){
-        return characters_count.size();
-    }
-    public Integer countUniqueCharacters(){
-        for (String word : characters_count){
-            characters_unique.put(word, characters_unique.getOrDefault(word, 0 ) + 1);
-        } return characters_unique.size();
+    public Statistic(String sentence) {
+        this.charactersUnique = new HashMap<>();
+        this.wordsUnique = new HashMap<>();
+        this.charactersCount = Arrays.asList(Optional.ofNullable(sentence).orElse("").split("(?!^)"));
+        this.wordsCount = Arrays.asList(Optional.ofNullable(sentence).orElse("").split("\\s+"));
     }
 
+    public long countTotalWords() {
+        return wordsCount.size();
+    }
+
+    public long countUniqueWords() {
+        wordsUnique.clear();
+        wordsCount.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((word, count) -> wordsUnique.put(word, count));
+        return wordsUnique.size();
+    }
+
+    public long countTotalCharacters() {
+        return charactersCount.size();
+    }
+
+    public long countUniqueCharacters() {
+        charactersUnique.clear();
+        charactersCount.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((character, count) -> charactersUnique.put(character, count));
+        return charactersUnique.size();
+    }
 }
